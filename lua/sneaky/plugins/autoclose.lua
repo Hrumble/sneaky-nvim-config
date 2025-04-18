b8838b1b71a1b6d9db8a85b87f38313edcd6ba7c
nolan <nolan.gero@gmail.com>
2025-04-18 19:42:31 +0200

    Modified autoclose to not work in text, md and command mode

--- lua/sneaky/plugins/autoclose.lua
+++ lua/sneaky/plugins/autoclose.lua
@@ -1,6 +1,12 @@
 return {
 	"m4xshen/autoclose.nvim",
 	config = function()
-		require('autoclose').setup()
+		require('autoclose').setup({
+			options = {
+				disabled_filetypes = {"text", "markdown"},
+				escape = true,
+				disable_command_mode = true,
+			}
+		})
 	end
 }
