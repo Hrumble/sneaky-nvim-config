0662499bbdf7be49312e2e7be611f39414ad4d4e
nolan <nolan.gero@gmail.com>
2025-04-18 22:02:57 +0200

    Set up toggleterm to handle multiple terminals

--- lua/sneaky/core/mappings.lua
+++ lua/sneaky/core/mappings.lua
@@ -1,10 +1,30 @@
--- Sets split screen movement to use Alt
 local keymap = vim.keymap
-keymap.set('n', '<A-j>', '<C-w>j')
-keymap.set('n', '<A-k>', '<C-w>k')
-keymap.set('n', '<A-h>', '<C-w>h')
-keymap.set('n', '<A-l>', '<C-w>l')
-keymap.set('n', 'ca', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+-- Sets leader key
+keymap.set({'n', 'v'}, '<Space>', '<Nop>')
+vim.g.mapleader = " "
 
+-- Sets split screen movement to use Alt
+keymap.set({'n', 't'}, '<A-j>', '<C-w>j')
+keymap.set({'n', 't'}, '<A-k>', '<C-w>k')
+keymap.set({'n', 't'}, '<A-h>', '<C-w>h')
+keymap.set({'n', 't'}, '<A-l>', '<C-w>l')
+
+-- maps code actions
+keymap.set('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', { noremap = true, silent = true })
+
+-- maps to reload nvim conf
+keymap.set('n', '<Leader>rr', '<cmd>source<cr>')
+
+-- Term keymaps
+vim.keymap.set({'t', 'n'}, '<A-t>', '<cmd>ToggleTermToggleAll<cr>')
+vim.keymap.set({'t', 'n'}, '<A-1>', '<cmd>1ToggleTerm direction=vertical size=200<cr>')
+vim.keymap.set({'t', 'n'}, '<A-2>', '<cmd>2ToggleTerm direction=vertical size=200<cr>')
+vim.keymap.set({'t', 'n'}, '<A-3>', '<cmd>3ToggleTerm direction=vertical size=200<cr>')
+vim.keymap.set({'t', 'n'}, '<A-4>', '<cmd>4ToggleTerm direction=vertical size=200<cr>')
+vim.keymap.set({'t', 'n'}, '<A-5>', '<cmd>5ToggleTerm direction=vertical size=200<cr>')
+vim.keymap.set({'t', 'n'}, '<A-6>', '<cmd>6ToggleTerm direction=vertical size=200<cr>')
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+
+-- Sets y to copy to clipboard
 vim.cmd("set clipboard=unnamedplus")
 
--- lua/sneaky/plugins/toggleterm.lua
+++ lua/sneaky/plugins/toggleterm.lua
@@ -2,11 +2,11 @@ return {
 	'akinsho/toggleterm.nvim',
 	version = '*',
 	config = function()
-		require("toggleterm").setup({
-			close_on_exit = true,
-			open_mapping = [[<A-t>]],
-			shell = vim.opt.shell:get(),
-			direction = "float",
+		require('toggleterm').setup({
+			direction = 'vertical',
+			winbar = {
+				enabled = false
+			}
 		})
 	end
 }
