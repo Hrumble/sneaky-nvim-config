ca0dfd4dc08430a141f91b57a20d0cb0cb9e4967
nolan <nolan.gero@gmail.com>
2025-04-20 11:53:39 +0200

    Changed terminal layouts from vertical to horizontal

--- lua/sneaky/core/mappings.lua
+++ lua/sneaky/core/mappings.lua
@@ -25,12 +25,12 @@ keymap.set('n', '<Leader>xc', '<cmd>Trouble diagnostics toggle focus=true filter
 
 -- Term keymaps
 vim.keymap.set({ "t", "n" }, "<A-t>", "<cmd>ToggleTermToggleAll<cr>")
-vim.keymap.set({ "t", "n" }, "<A-1>", "<cmd>1ToggleTerm direction=vertical size=100 name=1<cr>")
-vim.keymap.set({ "t", "n" }, "<A-2>", "<cmd>2ToggleTerm direction=vertical size=100 name=2<cr>")
-vim.keymap.set({ "t", "n" }, "<A-3>", "<cmd>3ToggleTerm direction=vertical size=100 name=3<cr>")
-vim.keymap.set({ "t", "n" }, "<A-4>", "<cmd>4ToggleTerm direction=vertical size=100 name=4<cr>")
-vim.keymap.set({ "t", "n" }, "<A-5>", "<cmd>5ToggleTerm direction=vertical size=100 name=5<cr>")
-vim.keymap.set({ "t", "n" }, "<A-6>", "<cmd>6ToggleTerm direction=vertical size=100 name=6<cr>")
+vim.keymap.set({ "t", "n" }, "<A-1>", "<cmd>1ToggleTerm direction=horizontal size=15 name=1<cr>")
+vim.keymap.set({ "t", "n" }, "<A-2>", "<cmd>2ToggleTerm direction=horizontal size=15 name=2<cr>")
+vim.keymap.set({ "t", "n" }, "<A-3>", "<cmd>3ToggleTerm direction=horizontal size=15 name=3<cr>")
+vim.keymap.set({ "t", "n" }, "<A-4>", "<cmd>4ToggleTerm direction=horizontal size=15 name=4<cr>")
+vim.keymap.set({ "t", "n" }, "<A-5>", "<cmd>5ToggleTerm direction=horizontal size=15 name=5<cr>")
+vim.keymap.set({ "t", "n" }, "<A-6>", "<cmd>6ToggleTerm direction=horizontal size=15 name=6<cr>")
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 
 -- Sets y to copy to clipboard
