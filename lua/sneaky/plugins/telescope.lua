bcf7c35a848b95d81ce250485a467fc8924687c4
nolan <nolan.gero@gmail.com>
2025-04-23 20:25:18 +0200

    Added autocommenting, tried to optimize the config, failed.

--- lua/sneaky/plugins/telescope.lua
+++ lua/sneaky/plugins/telescope.lua
@@ -1,28 +1,28 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', 
-		tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		cmd = { "Telescope colorscheme", "Telescope find_files", "Telescope live_grep" },
 		config = function()
 			local telescope = require("telescope")
 			local actions = require("telescope.actions")
 			telescope.setup({
 				defaults = {
-					layout_stategy = 'vertical',
+					layout_stategy = "vertical",
 					mappings = {
 						i = {
 							["<A-h>"] = actions.select_horizontal,
 							["<A-v>"] = actions.select_vertical,
-					},
+						},
 						n = {
 							["<A-h>"] = actions.select_horizontal,
-							["<A-v>"] = actions.select_vertical
-
-						}
-					}
-				}
+							["<A-v>"] = actions.select_vertical,
+						},
+					},
+				},
 			})
-		end
+		end,
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
@@ -30,12 +30,11 @@ return {
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown{
-						}
-					}
-				}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
 			require("telescope").load_extension("ui-select")
-		end
-	}
+		end,
+	},
 }
