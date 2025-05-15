4f3377f2ebefca31aba1b73cf4fee5679494cb47
Hrumble <sneaky@mail.com>
2025-05-15 21:08:07 +0200

    Setup smartsplit with telescope

--- lua/sneaky/plugins/telescope.lua
+++ lua/sneaky/plugins/telescope.lua
@@ -6,18 +6,26 @@ return {
 		cmd = { "Telescope colorscheme", "Telescope find_files", "Telescope live_grep" },
 		config = function()
 			local telescope = require("telescope")
-			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+
+			local smart_split = function()
+				local smart = require("smartsplit.smart-split")
+				local selection = action_state.get_selected_entry()
+
+				smart.smart_split()
+				vim.cmd("edit " .. selection.path)
+
+			end
+
 			telescope.setup({
 				defaults = {
 					layout_stategy = "vertical",
 					mappings = {
 						i = {
-							["<A-h>"] = actions.select_horizontal,
-							["<A-v>"] = actions.select_vertical,
+							["<A-s>"] = smart_split,
 						},
 						n = {
-							["<A-h>"] = actions.select_horizontal,
-							["<A-v>"] = actions.select_vertical,
+							["<A-s>"] = smart_split,
 						},
 					},
 				},
