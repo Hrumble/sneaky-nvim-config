fc25cecf44d0228849f03ee427c0522f7805966c
Hrumble <sneaky@mail.com>
2025-09-18 18:46:03 +0200

    Readded render markdown

--- lua/sneaky/plugins/awareness.lua
+++ lua/sneaky/plugins/awareness.lua
@@ -3,21 +3,28 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		config = function()
-			require('treesitter-context').setup({
-
-			})
-		end
+			require("treesitter-context").setup({})
+		end,
 	},
 	{
-		'stevearc/aerial.nvim',
+		"stevearc/aerial.nvim",
 		opts = {},
 		-- Optional dependencies
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons"
+			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
 			require("aerial").setup({})
-		end
-	}
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 }
