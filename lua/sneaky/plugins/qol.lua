6b47fad7297426bbd046cac5327f8d58cdfe230e
Nolan GERO <nolan.gero@quietamstudio.fr>
2025-12-04 13:21:09 +0100

    feat: marker groups for notetaking

--- lua/sneaky/plugins/qol.lua
+++ lua/sneaky/plugins/qol.lua
@@ -88,5 +88,22 @@ return {
 				}
 			})
 		end
+	},
+	{
+		"jameswolensky/marker-groups.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",       -- Required
+			"nvim-telescope/telescope.nvim", -- Optional: Telescope picker
+			-- mini.pick is part of mini.nvim; this plugin vendors mini.nvim for tests,
+			-- but you can also install mini.nvim explicitly to use mini.pick system-wide
+			-- "nvim-mini/mini.nvim",
+		},
+		config = function()
+			require("marker-groups").setup({
+				-- Default picker is 'vim' (built-in vim.ui)
+				-- Accepted values: 'vim' | 'snacks' | 'fzf-lua' | 'mini.pick' | 'telescope'
+				picker = 'telescope',
+			})
+		end,
 	}
 }
