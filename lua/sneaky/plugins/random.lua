de99484b8e9b791e62a9291c5b8d4b85bcaa9518
Nolan GERO <nolan.gero@quietamstudio.fr>
2025-12-04 13:34:02 +0100

    feat: triforce rpg stats

--- lua/sneaky/plugins/random.lua
+++ lua/sneaky/plugins/random.lua
@@ -2,5 +2,18 @@ return {
 	{
 		"eandrju/cellular-automaton.nvim",
 		event = "VeryLazy"
-	}
+	},
+	-- A fun RPG stat tracker
+	{
+		'gisketch/triforce.nvim',
+		dependencies = { 'nvzone/volt' },
+		config = function()
+			require('triforce').setup({
+				-- Optional: Add your configuration here
+				keymap = {
+					show_profile = '<leader>tp', -- Open profile with <leader>tp
+				},
+			})
+		end,
+	},
 }
