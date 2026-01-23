ec1bc29d778b6a375cc4436cc5d71435b52f3b41
Nolan GERO <nolan.gero@quietamstudio.fr>
2026-01-23 10:38:18 +0100

    feat: more dap keymaps

--- lua/sneaky/plugins/debugging.lua
+++ lua/sneaky/plugins/debugging.lua
@@ -1,64 +1,59 @@
-return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+  return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-		dapui.setup()
+    dapui.setup()
 
-		-- set up when dapui opens and closes
-		dap.listeners.before.attach.dapui_config = function ()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function ()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function ()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function ()
-			dapui.close()
-		end
+    -- set up when dapui opens and closes
+    dap.listeners.before.attach.dapui_config = function ()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function ()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function ()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function ()
+      dapui.close()
+    end
 
-		-- keymaps
-		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {desc = "toggles a debugging breakpoint"})
-		vim.keymap.set("n", "<Leader>dn", dap.continue, {desc = "Next debugging step"})
+    -- keymaps
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {desc = "toggles a debugging breakpoint"})
+    vim.keymap.set("n", "<Leader>dn", dap.next, {desc = "DAP Next"})
+    vim.keymap.set("n", "<Leader>df", dap.step, {desc = "DAP Step"})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {desc = "DAP Continue"})
+
+  ----------------------------------------------------------------------
+  --                             Adapters                             --
+  ----------------------------------------------------------------------
 
     dap.adapters.gdb = {
       type = "executable",
       command = "gdb",
       args = {"--interpreter=dap", "--eval-command", "set print pretty on"}
     }
-		-- Python debug adapter (requires debugpy)
-		dap.adapters.python = function(cb, config)
-			if config.request == 'attach' then
-				---@diagnostic disable-next-line: undefined-field
-				local port = (config.connect or config).port
-				---@diagnostic disable-next-line: undefined-field
-				local host = (config.connect or config).host or '127.0.0.1'
-				cb({
-					type = 'server',
-					port = assert(port, '`connect.port` is required for a python `attach` configuration'),
-					host = host,
-					options = {
-						source_filetype = 'python',
-					},
-				})
-			else
-				cb({
-					type = 'executable',
-					command = "python",
-					args = { '-m', 'debugpy.adapter' },
-					options = {
-						source_filetype = 'python',
-					},
-				})
-			end
-		end
+
+    dap.adapters.dart = {
+      type = 'executable',
+      command = 'dart',    -- if you're using fvm, you'll need to provide the full path to dart (dart.exe for windows users), or you could prepend the fvm command
+      args = { 'debug_adapter' },
+      -- windows users will need to set 'detached' to false
+      options = {
+        detached = false,
+      }
+    }
+
+  ----------------------------------------------------------------------
+  --                          Configurations                          --
+  ----------------------------------------------------------------------
+
     dap.configurations.c = {
       {
         name = "Launch",
@@ -95,70 +90,34 @@ return {
         cwd = '${workspaceFolder}'
       }
     }
-		dap.configurations.python = {
-			{
-				-- The first three options are required by nvim-dap
-				type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
-				request = 'launch';
-				name = "Launch file";
-
-				-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-				program = "${file}"; -- This configuration will launch the current file if used.
-				pythonPath = function()
-					-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-					-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-					-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-						local cwd = vim.fn.getcwd()
-						if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-							return cwd .. '/venv/bin/python'
-						elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-							return cwd .. '/.venv/bin/python'
-						else
-							return 'python'
-						end
-					end,
-			},
-		}
-
-		-- Dart debug adapter + flutter
-		dap.configurations.dart = {
-			{
-				type = "dart",
-				request = "launch",
-				name = "Launch dart",
-				dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
-				flutterSdkPath = "/opt/flutter/bin/flutter",                  -- ensure this is correct
-				program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
-				cwd = "${workspaceFolder}",
-			},
-			{
-				type = "flutter",
-				request = "launch",
-				name = "Launch flutter",
-				dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
-				flutterSdkPath = "/opt/flutter/bin/flutter",             -- ensure this is correct
-				program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
-				cwd = "${workspaceFolder}",
-			}
-		}
-		dap.adapters.dart = {
-			type = 'executable',
-			command = 'dart',    -- if you're using fvm, you'll need to provide the full path to dart (dart.exe for windows users), or you could prepend the fvm command
-			args = { 'debug_adapter' },
-			-- windows users will need to set 'detached' to false
-			options = {
-				detached = false,
-			}
-		}
-		dap.adapters.flutter = {
-			type = 'executable',
-			command = 'flutter',   -- if you're using fvm, you'll need to provide the full path to flutter (flutter.bat for windows users), or you could prepend the fvm command
-			args = { 'debug_adapter' },
-			-- windows users will need to set 'detached' to false
-			options = {
-				detached = false,
-			}
-		}
-	end,
-}
+    dap.configurations.dart = {
+      {
+        type = "dart",
+        request = "launch",
+        name = "Launch dart",
+        dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
+        flutterSdkPath = "/opt/flutter/bin/flutter",                  -- ensure this is correct
+        program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "flutter",
+        request = "launch",
+        name = "Launch flutter",
+        dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
+        flutterSdkPath = "/opt/flutter/bin/flutter",             -- ensure this is correct
+        program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
+        cwd = "${workspaceFolder}",
+      }
+    }
+    dap.adapters.flutter = {
+      type = 'executable',
+      command = 'flutter',   -- if you're using fvm, you'll need to provide the full path to flutter (flutter.bat for windows users), or you could prepend the fvm command
+      args = { 'debug_adapter' },
+      -- windows users will need to set 'detached' to false
+      options = {
+        detached = false,
+      }
+    }
+  end,
+  }
