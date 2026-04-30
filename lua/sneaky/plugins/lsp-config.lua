0d12372d1afbb8a41443f765adc994fa44280782
Hrumble <nolan.gero@gmail.com>
2026-04-30 21:54:27 +0200

    feat: ts_ls on javascript files

--- lua/sneaky/plugins/lsp-config.lua
+++ lua/sneaky/plugins/lsp-config.lua
@@ -55,7 +55,7 @@ return {
           filetypes = { "css", "scss", "less" },
         },
         ts_ls = {
-          filetypes = { "typescriptreact", "typescript" }, -- or use "tsserver" for default name
+          filetypes = { "typescriptreact", "typescript", "javascript" }, -- or use "tsserver" for default name
         },
 
         jdtls = {
